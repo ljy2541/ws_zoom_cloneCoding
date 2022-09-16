fe4d8d99189d1cf7f95562fa5902814df45f22b1
jylee-idv <jylee@idevel.kr>
2022-09-16 18:20:51 +0900

    인풋을 활용한 소켓통신

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,25 @@
-alert('hello world') 
+
+const messageList = document.querySelector('ul')
+const messageForm = document.querySelector('form')
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+socket.addEventListener('open', () => {
+    console.log('Connected to Server✔');
+})
+
+socket.addEventListener('message', (message) => {
+    console.log('Just got this: ' + message.data +  " from the Server");
+})
+
+socket.addEventListener("close", () => {
+    console.log("Disconnect from Server❌")
+})
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector('input');
+    socket.send(input.value)
+    input.value = ''
+}
+
+messageForm.addEventListener('submit', handleSubmit)
--- src/server.js
+++ src/server.js
@@ -7,11 +7,22 @@ const app = express()
 app.set("view engine", "pug")
 app.set("views", __dirname + '/views')
 app.use('/public', express.static(__dirname + '/public'))
-app.get('/', (req, res) => res.render('home'))
+app.get('/', (_, res) => res.render('home'))
+app.get('/*', (_, res) => res.redirect('/'))
 
 const handleListen = () => console.log('Listening on http://localhost:3000')
 
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
+
+wss.on("connection", (socket) => {
+    console.log('Connected to Browser✔');
+    socket.on('close', () => console.log('Disconnect to Browser❌'))
+    socket.on('message', (message) => {
+        socket.send(message.toString('utf-8'));
+    })    
+})
+
+
 
 server.listen(3000, handleListen)
